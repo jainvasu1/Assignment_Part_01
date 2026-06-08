27d45aab9e0805c3688d27e6ab4993ca7d04b4a3
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-08 15:03:23 +0530

    Program5(A1)

---
+++ A1/program5.dart
@@ -0,0 +1,11 @@
+//Prints: "In 5 years I will be <age+5> and it will be <currentYear+5>.
+
+final int currentYear = DateTime.now().year;
+
+void main() {
+  String name = "Vasudha";
+
+  int age = 20;
+
+  print("In 5 years I will be ${age + 5} and it will be ${currentYear + 5}");
+}
