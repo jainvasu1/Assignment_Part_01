ad06cbb211d70838f4a4e1fe1c17f2e5a5bcc03b
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-08 14:52:47 +0530

    first commit

---
+++ A1/program1.dart
@@ -0,0 +1,13 @@
+//Declares your name (String), age (int), gpa (double), and isStudent (bool) using explicit types.
+
+void main() {
+  String name = "Vasudha";
+  int age = 20;
+  double gpa = 8.45;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("GPA: $gpa");
+  print("Is Student: $isStudent");
+}
---
+++ A1/program4.dart
@@ -0,0 +1,10 @@
+//Prints: "Hi, I'm <name>, age <age>, GPA <gpa>. Student: <isStudent>."
+
+void main() {
+  String name = "Vasudha";
+  int age = 20;
+  double gpa = 8.45;
+  bool isStudent = true;
+
+  print("Hi, I'm $name, $age, $gpa. Student:$isStudent");
+}
