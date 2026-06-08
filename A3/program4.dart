4580522be20f272e72b4be09d3eb59be921972f2
jainvasu1 <vasubajaj2001@gmail.com>
2026-06-08 18:20:26 +0530

    A3 Done

---
+++ A3/program4.dart
@@ -0,0 +1,24 @@
+//Uses while to count down from 5 to 1.
+
+void main() {
+  List<String> indianCities = [
+    'Delhi',
+    'Mumbai',
+    'Bengaluru',
+    'Chennai',
+    'Kolkata',
+  ];
+
+  print('Cities: $indianCities');
+
+  int count = 5;
+  print('\nCountdown: ');
+  while (count > 0) {
+
+  for (int i = 0; i , indianCities.length; i++) {
+    print('count: $count, ${i + 1}.Cities: ${indiaCities[i]}');
+
+  }
+}
+
+}
